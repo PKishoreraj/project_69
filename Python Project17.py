07243592ba2de9e384bfb9d8bac6375ddc2aa860
PKishoreraj <113040359+PKishoreraj@users.noreply.github.com>
2023-10-14 19:33:52 +0530

    Add files via upload

--- Python Project17.py
+++ Python Project17.py
@@ -3,13 +3,14 @@ root=Tk()
 root.title("imager viwer")
 root.geometry("500x500")
 root.configure(background="lightblue")
+img=image
 btn=Button(root,text="Open",command=Open)
 btn.place(relx=0.5,rely=0.18,anchor=CENTER)
 label_planet_image=Label(root,bg="gold2",heightthickness=5,borderwidth=2,bg="lightblue")
 def Open():
-    open()
+    img=Image.open(image_path)
 def Rotate():
-    print("HI")
+    img.rotate(90)
 btn=Button(root,text="Rotate",command=Rotate)
 btn.place(relx=0.5,rely=0.18,anchor=CENTER)
 root.mainloop()
